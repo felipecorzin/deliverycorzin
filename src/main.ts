d8d0d1fec4d3a7b4f76649adaa182f897c4efc9f
felipecorzin <jfelipestc@gmail.com>
2024-12-02 18:48:51 +0100

    commit 02/12/2024 19:00

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  // Directorio Público
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  await app.listen(parseInt(process.env.PORT) || 3000, () => {
+    console.log(`Server is running on port 3000`);
+  });
 }
 bootstrap();
